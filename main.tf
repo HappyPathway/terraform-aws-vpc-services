fcdee22874c36c1bcdb5f6223ebf2286ece9777a
David Arnold <10138997+djaboxx@users.noreply.github.com>
2024-09-25 11:17:03 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,1 @@
-# This is the main configuration file for the Terraform module.
-data "external" "get_services" {
-  program = ["python", "${path.module}/get_services.py"]
 
-  query = {
-    # arbitrary map from strings to strings, passed
-    # to the external program as the data query.
-    region = var.region
-  }
-}
