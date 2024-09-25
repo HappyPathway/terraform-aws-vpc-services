da1b2867189705b699e662fd036ad4766d8b58c4
Dave Arnold <dave@roknsound.com>
2024-09-25 11:34:32 -0700

    Refactor get_services.py and main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,16 @@
-data "local_file" "example" {
+data "local_file" "services" {
   filename = var.services
 }
 
 locals {
-  services = jsondecode(data.local_file.example.content)
+  services = jsondecode(data.local_file.services.content)
+}
+
+data "aws_vpc_endpoint_service" "service" {
+  for_each = local.services
+  service  = each.value
+}
+
+output "service" {
+  value = data.aws_vpc_endpoint_service.service
 }
